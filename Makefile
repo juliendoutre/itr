3bdccf7b941fef4a80f9a211e467c6033b9f7704
Julien Doutre <jul.doutre@gmail.com>
2020-03-03 09:39:08 +0100

    Add new looper with threads

--- Makefile
+++ Makefile
@@ -3,11 +3,15 @@ all: clean build tests
 clean:
 	rm -rf build/*
 
-build: build_lib fifo timer looper looper2 t_looper
+build: build_lib fifo build_td_1 build_td_2
 
 build_lib:
 	g++ -c src/time.cpp -I . -o build/itr.o -O3 -Wall -Wextra
 	ar rcs lib/itr.a build/itr.o
+
+build_td_1: timer looper looper2
+
+build_td_2: t_looper t_looper2
 
 fifo:
 	gcc src/fifo/main.c -o bin/fifo -O3 -Wall -Wextra
@@ -23,6 +27,9 @@ looper2:
 
 t_looper:
 	g++ src/t_looper/main.cpp lib/itr.a -I . -lrt -pthread -o bin/t_looper -O3 -Wall -Wextra
+
+t_looper2:
+	g++ src/t_looper2/main.cpp lib/itr.a -I . -lrt -pthread -o bin/t_looper2 -O3 -Wall -Wextra
 
 tests: test_time
 
---
+++ src/t_looper2/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <pthread.h>
+#include "include/time.hpp"
+
+void incr(unsigned int nLoops, volatile double *pCounter)
+{
+    for (unsigned int i = 0; i < nLoops; i++)
+    {
+        *pCounter += 1.0;
+    }
+}
+
+struct Data
+{
+    volatile unsigned int nLoops;
+    volatile double *pCounter;
+};
+
+void *call_incr(void *v_data)
+{
+    Data *p_data = (Data *)v_data;
+    incr(p_data->nLoops, p_data->pCounter);
+
+    return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+    volatile unsigned int nLoops = 0;
+    unsigned int nTasks = 0;
+
+    if (argc > 2)
+    {
+        nLoops = atoi(argv[1]);
+        nTasks = atoi(argv[2]);
+    }
+
+    volatile double counter = 0.0;
+    Data data = Data{nLoops, &counter};
+    pthread_t threads[nTasks];
+
+    struct timespec start_ts = timespec_now();
+
+    for (unsigned int i = 0; i < nTasks; i++)
+    {
+        pthread_create(&threads[i], NULL, call_incr, &data);
+    }
+
+    for (unsigned int i = 0; i < nTasks; i++)
+    {
+        pthread_join(threads[i], NULL);
+    }
+
+    struct timespec end_ts = timespec_now();
+
+    std::cout << "counter value: " << counter << std::endl;
+    std::cout << "function execution time: " << end_ts - start_ts << std::endl;
+
+    return EXIT_SUCCESS;
+}
