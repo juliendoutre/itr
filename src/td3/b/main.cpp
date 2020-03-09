b0aee428687ffca0ec25da5ff7c06e111564c93d
Julien Doutre <jul.doutre@gmail.com>
2020-03-09 19:50:04 +0100

    Complete Chrono class example

--- src/td3/b/main.cpp
+++ src/td3/b/main.cpp
@@ -1,9 +1,13 @@
 #include "countDown.hpp"
+#include <chrono>
+#include <thread>
 
 int main()
 {
-    CountDown c = CountDown(105);
-    c.start(1000);
+    CountDown c = CountDown(10);
+    c.start(100);
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
     return 0;
 }
