f1b1a9faf0a0e862d0aaa4837949d2265087bc8b
Julien Doutre <jul.doutre@gmail.com>
2020-03-23 17:08:08 +0100

    Use lib wait function to avoid using other std dep

--- src/td3/b/main.cpp
+++ src/td3/b/main.cpp
@@ -1,13 +1,16 @@
 #include "CountDown.hpp"
-#include <chrono>
-#include <thread>
+#include "itr/time.hpp"
 
 int main()
 {
     CountDown c = CountDown(10);
     c.start(100);
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    timespec remaining_time = timespec_from_ms(1000);
+    while (remaining_time > timespec_from_ms(10))
+    {
+        remaining_time = timespec_wait(remaining_time);
+    }
 
     return 0;
 }
