91c59e8f0ac6e4d71c889bda8556a421ef61b3b4
Julien 'Orphus' Doutre <julien.doutre@student.ecp.fr>
2020-04-06 12:53:16 +0200

    Fix extensions

--- src/td6/doc.h
+++ src/td6/doc.h
@@ -6,11 +6,11 @@
  * @section Purpose
  * TODO
  * @section files Related files
- * -  @ref td6_main "src/td6/main.c"
- * -  @ref td6_active_calc "src/td6/ActiveCalc.c"
- * -  @ref lib_active_object "src/td6/ActiveObject.c"
- * -  @ref td6_calculator "src/td6/Calculator.c"
- * -  @ref td6_crunch_req "src/td6/CrunchReq.c"
- * -  @ref lib_request "src/td6/Request.c"
- * -  @ref td6_terminal_req "src/td6/TerminalReq.c"
+ * -  @ref td6_main "src/td6/main.cpp"
+ * -  @ref td6_active_calc "src/td6/ActiveCalc.cpp"
+ * -  @ref lib_active_object "src/td6/ActiveObject.cpp"
+ * -  @ref td6_calculator "src/td6/Calculator.cpp"
+ * -  @ref td6_crunch_req "src/td6/CrunchReq.cpp"
+ * -  @ref lib_request "src/td6/Request.cpp"
+ * -  @ref td6_terminal_req "src/td6/TerminalReq.cpp"
  * */
